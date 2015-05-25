afbd82faee9c346f389b37dda6cf14d26b957f0c
Serhii Prokhorenko <grey.psv@gmail.com>
2015-05-25 23:04:44 +0300

    Cut download function. Simple activate method.

--- admin/install-plugins.php
+++ admin/install-plugins.php
@@ -1,60 +1,49 @@
 <?php
 
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
 function mm_get_plugins($plugins)
 {
     $args = array(
-            'path' => ABSPATH.'wp-content/plugins/',
-            'preserve_zip' => false
+            'path'          => ABSPATH . 'wp-content/plugins/',
+            'preserve_zip'  => true
     );
 
     foreach($plugins as $plugin)
     {
-            mm_plugin_download($plugin['path'], $args['path'].$plugin['name'].'.zip');
-            mm_plugin_unpack($args, $args['path'].$plugin['name'].'.zip');
-            mm_plugin_activate($plugin['install']);
+        mm_plugin_unpack($args, get_template_directory() . '/lib/plugins/' . $plugin['name'].'.zip');
+        mm_plugin_activate($plugin['install']);
     }
 }
-function mm_plugin_download($url, $path) 
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    if(file_put_contents($path, $data))
-            return true;
-    else
-            return false;
-}
+
 function mm_plugin_unpack($args, $target)
 {
     if($zip = zip_open($target))
     {
-            while($entry = zip_read($zip))
+        while($entry = zip_read($zip))
+        {
+            $is_file = substr(zip_entry_name($entry), -1) == '/' ? false : true;
+            $file_path = $args['path'].zip_entry_name($entry);
+            if($is_file)
             {
-                    $is_file = substr(zip_entry_name($entry), -1) == '/' ? false : true;
-                    $file_path = $args['path'].zip_entry_name($entry);
-                    if($is_file)
-                    {
-                            if(zip_entry_open($zip,$entry,"r")) 
-                            {
-                                    $fstream = zip_entry_read($entry, zip_entry_filesize($entry));
-                                    file_put_contents($file_path, $fstream );
-                                    chmod($file_path, 0777);
-                                    //echo "save: ".$file_path."<br />";
-                            }
-                            zip_entry_close($entry);
-                    }
-                    else
-                    {
-                            if(zip_entry_name($entry))
-                            {
-                                    mkdir($file_path);
-                                    chmod($file_path, 0777);
-                                    //echo "create: ".$file_path."<br />";
-                            }
-                    }
+                if(zip_entry_open($zip,$entry,"r")) 
+                {
+                    $fstream = zip_entry_read($entry, zip_entry_filesize($entry));
+                    file_put_contents($file_path, $fstream );
+                    chmod($file_path, 0777);
+                }
+                zip_entry_close($entry);
             }
-            zip_close($zip);
+            else
+            {
+                if(zip_entry_name($entry))
+                {
+                    mkdir($file_path);
+                    chmod($file_path, 0777);
+                }
+            }
+        }
+        zip_close($zip);
     }
     if($args['preserve_zip'] === false)
     {
@@ -68,22 +57,18 @@ function mm_plugin_activate($installer)
 
     if(!in_array($plugin, $current))
     {
-            $current[] = $plugin;
-            sort($current);
-            do_action('activate_plugin', trim($plugin));
-            update_option('active_plugins', $current);
-            do_action('activate_'.trim($plugin));
-            do_action('activated_plugin', trim($plugin));
-            return true;
+        activate_plugin($plugin);
     }
     else
-            return false;
+        return false;
 }
 
-$plugins = array(
-    // array('name' => 'Display Widgets', 'path' => 'https://downloads.wordpress.org/plugin/display-widgets.2.03.zip', 'install' => 'display-widgets/display-widgets.php'),
-    // array('name' => 'Polylang', 'path' => 'https://downloads.wordpress.org/plugin/polylang.1.7.5.zip', 'install' => 'polylang/polylang.php'),
-);
-mm_get_plugins($plugins);
 
+$plugins = array(
+    array('name' => 'display-widgets', 'path' => 'https://downloads.wordpress.org/plugin/display-widgets.2.03.zip', 'install' => 'display-widgets/display-widgets.php'),
+    array('name' => 'polylang', 'path' => 'https://downloads.wordpress.org/plugin/polylang.1.7.5.zip', 'install' => 'polylang/install/install.php'),
+);
+if (!is_plugin_active( 'display-widgets/display-widgets.php') && !is_plugin_active( 'polylang/polylang.php' )) {
+    mm_get_plugins($plugins);
+}
 ?>
