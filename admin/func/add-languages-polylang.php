89639576ce981c9184e2a88c143279af06dcf234
Serhii Prokhorenko <grey.psv@gmail.com>
2015-05-29 13:02:10 +0300

    check if language install already

--- admin/func/add-languages-polylang.php
+++ admin/func/add-languages-polylang.php
@@ -6,14 +6,28 @@ if ( !function_exists('add_languages_polylang') ) {
 		$options = get_option('polylang');
 		$model = new PLL_Admin_Model($options);
 		$languages = array(
-			array( 'name' => 'Русский', 'slug' => 'ru', 'locale' => 'ru_RU', 'rtl' => 0, 'term_group' => 3),
-			array( 'name' => 'Українська', 'slug' => 'uk', 'locale' => 'uk', 'rtl' => 0, 'term_group' => 4),
+			array( 'name' => 'English', 'slug' => 'en', 'locale' => 'en_GB', 'rtl' => 0, 'term_group' => 1),
+			array( 'name' => 'Deutsch', 'slug' => 'de', 'locale' => 'de_DE', 'rtl' => 0, 'term_group' => 2),
 		);
 		foreach ($languages as $language) {
-			$model->add_language($language);
+			$language_installed = pll_is_language_installed($language['slug']);
+			if (!$language_installed) {
+				$model->add_language($language);
+			}
 		}
-
 	}
 }
+
+function pll_is_language_installed($language_code) {
+	global $polylang;
+	$languages = $polylang->model->get_languages_list();
+	foreach ($languages as $language) {
+		if ($language->slug == $language_code) {
+			return true;
+		}
+	}
+	return false;
+}
+
 add_action( 'after_setup_theme', 'add_languages_polylang' );
 ?>
