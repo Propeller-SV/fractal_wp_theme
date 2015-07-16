dcff3b3079648b1f6d8f76e636365b63287f45ef
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-16 10:53:05 +0300

    Footer menu and social icons DRY rewrite. Convert indentation to tab

--- footer.php
+++ footer.php
@@ -20,33 +20,15 @@
                         $menu_id = wp_create_nav_menu( 'Footer fractal menu' );
 
                         // Set up default menu items
-                        wp_update_nav_menu_item($menu_id, 0, array(
-                            'menu-item-title' =>  __('Home'), 
-                            'menu-item-object' => 'page',
-                            'menu-item-object-id' => get_page_by_title('home')->ID,
-                            'menu-item-type' => 'post_type',
-                            'menu-item-status' => 'publish'));
-
-                        wp_update_nav_menu_item($menu_id, 0, array(
-                            'menu-item-title' =>  __('Software Engineering'), 
-                            'menu-item-object' => 'page',
-                            'menu-item-object-id' => get_page_by_title('Software Engineering')->ID,
-                            'menu-item-type' => 'post_type',
-                            'menu-item-status' => 'publish'));
-
-                        wp_update_nav_menu_item($menu_id, 0, array(
-                            'menu-item-title' =>  __('Company'),
-                            'menu-item-object' => 'page',
-                            'menu-item-object-id' => get_page_by_title('company')->ID,
-                            'menu-item-type' => 'post_type',
-                            'menu-item-status' => 'publish'));
-
-                        wp_update_nav_menu_item($menu_id, 0, array(
-                            'menu-item-title' =>  __('Career'), 
-                            'menu-item-object' => 'page',
-                            'menu-item-object-id' => get_page_by_title('career')->ID,
-                            'menu-item-type' => 'post_type',
-                            'menu-item-status' => 'publish'));
+                        $pages = ['home', 'software engineering', 'company', 'career'];
+                        for ($i=0; $i<count($pages); $i++) {
+                            wp_update_nav_menu_item($menu_id, 0, array(
+                            'menu-item-title'       => $pages[$i],
+                            'menu-item-object'      => 'page',
+                            'menu-item-object-id'   => get_page_by_title($pages[$i])->ID,
+                            'menu-item-type'        => 'post_type',
+                            'menu-item-status'      => 'publish'));
+                        }
                     };
 
                     wp_nav_menu( array(
@@ -72,11 +54,18 @@
                 </div>
                 <div class="col-sm-8 col-md-6">
                     <ul class="nav nav-pills">
-                        <?php $options = (array)get_option('social_links'); ?>
-                        <li><a href="<?php echo esc_url($options['facebook']); ?>"><img src="<?php echo IMAGES; ?>/facebook.png"></a></li>
+                        <?php $options = (array)get_option('social_links');
+                        $icons = ['/facebook.png', '/twitter.png', '/linkedin.png', '/google.png'];
+                        for ($i=0; $i<count($icons); $i++) {
+                            ?>
+                            <li><a href="<?php if ($options) echo esc_url(array_values($options)[$i]); else echo '#'; ?>"><img src="<?php echo IMAGES . $icons[$i]; ?>"></a></li>
+                            <?php
+                        }
+                        ?>
+                        <!-- <li><a href="<?php echo esc_url($options['facebook']); ?>"><img src="<?php echo IMAGES; ?>/facebook.png"></a></li>
                         <li><a href="<?php echo esc_url($options['twitter']); ?>"><img src="<?php echo IMAGES; ?>/twitter.png"></a></li>
                         <li><a href="<?php echo esc_url($options['linked_in']); ?>"><img src="<?php echo IMAGES; ?>/linkedin.png"></a></li>
-                        <li><a href="<?php echo esc_url($options['google_plus']); ?>"><img src="<?php echo IMAGES; ?>/google.png"></a></li>
+                        <li><a href="<?php echo esc_url($options['google_plus']); ?>"><img src="<?php echo IMAGES; ?>/google.png"></a></li> -->
                     </ul>
                 </div>
             </div>
