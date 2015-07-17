8da711ff8660cf5c5d7d8eaf0744e6c64a6f15ca
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-17 18:14:10 +0300

    remove unnecessary default_top_nav

--- admin/func/menus/default_top_nav.php
+++
@@ -1,3 +0,0 @@
-<ul class='nav navbar-nav navbar-right'>					
-	<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/nav-menus.php"><?php _e('Please, create the Top Menu', 'fractal'); ?></a></li>
-</ul>
