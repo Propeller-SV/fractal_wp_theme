b042a65cee1fa981c83e15734ce0d3b6f701a7ca
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-18 13:11:12 +0300

    delete default primary and footer nav functions

--- admin/func/menus/default_footer_nav.php
+++
@@ -1,3 +0,0 @@
-<ul class='nav nav-pills pull-left'>
-	<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/nav-menus.php"><?php _e('Please, create the Footer fractal menu', 'fractal'); ?></a></li>
-</ul>
--- admin/func/menus/default_primary_nav.php
+++
@@ -1,3 +0,0 @@
-<ul class='navbar-main nav'>					
-	<li><a href="<?php bloginfo('wpurl'); ?>/wp-admin/nav-menus.php"><?php _e('Please, create the Primary fractal menu', 'fractal'); ?></a></li>
-</ul>
