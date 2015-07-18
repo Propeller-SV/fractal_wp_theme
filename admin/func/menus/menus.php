2a64498f54bd085c9aa4bc0435d8af70de70ccc5
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-18 13:19:41 +0300

    add footer and header nav menu creation functions to menus.php. Add custom link to /wp-admin/nav-menus.php for empty nav menu DE

--- admin/func/menus/menus.php
+++ admin/func/menus/menus.php
@@ -8,11 +8,113 @@ function register_my_menus() {
 }
 add_action( 'after_setup_theme', 'register_my_menus' );
 
-function default_primary_nav() {
-	require_once (THEMEFUNC . '/menus/default_primary_nav.php');
-}
+// define the wp_create_top_nav_menu callback
+function action_wp_create_top_nav_menu() {
 
-function default_footer_nav() {
-	require_once (THEMEFUNC . '/menus/default_footer_nav.php');
-}
-?>
+	$currentlang = get_bloginfo('language');
+	if ($currentlang !== "de-DE") :
+		// Check if the menu exists
+		$menu_exists = wp_get_nav_menu_object( 'Top menu EN' );
+
+		// If it doesn't exist, let's create it.
+		if( !$menu_exists ){
+			$menu_id = wp_create_nav_menu( 'Top menu EN' );
+			// Set up default menu items
+
+			$pages = ['Home', 'Software engineering', 'Company', 'Career'];
+			for ($i=0; $i<count($pages); $i++) {
+				wp_update_nav_menu_item($menu_id, 0, array(
+				'menu-item-title'		=> $pages[$i],
+				'menu-item-object'		=> 'page',
+				'menu-item-object-id'	=> get_page_by_title($pages[$i])->ID,
+				'menu-item-type'		=> 'post_type',
+				'menu-item-status'		=> 'publish'));
+			}
+		};
+		wp_nav_menu( array(
+			'theme_location'	=> 'primary',
+			'menu'				=> 'Top menu EN',
+			'container'			=> '',
+			'menu_class'		=> 'navbar-main nav text-uppercase'
+		));
+	elseif ($currentlang == "de-DE") :
+		// Check if the menu exists
+		$menu_exists = wp_get_nav_menu_object( 'Top menu DE' );
+
+		// If it doesn't exist, let's create it.
+		if( !$menu_exists ){
+			$menu_id = wp_create_nav_menu( 'Top menu DE' );
+
+			wp_update_nav_menu_item($menu_id, 0, array(
+				'menu-item-title'		=> __('Add menu items to Top menu DE here', 'fractal'),
+				'menu-item-url' 		=> get_bloginfo('wpurl') . '/wp-admin/nav-menus.php',
+				'menu-item-status'		=> 'publish')
+			);
+		};
+
+		wp_nav_menu( array(
+			'theme_location'	=> 'primary',
+			'menu'				=> 'Top menu DE',
+			'container'			=> '',
+			'menu_class'		=> 'navbar-main nav text-uppercase',
+			'fallback_cb'		=> 'default_primary_nav'
+		));
+	endif;
+};
+add_action( 'wp_create_top_nav_menu', 'action_wp_create_top_nav_menu' );
+
+// define the wp_create_footer_nav_menu callback
+function action_wp_create_footer_nav_menu() {
+
+	$currentlang = get_bloginfo('language');
+	if ($currentlang !== "de-DE") :
+		// Check if the menu exists
+		$menu_exists = wp_get_nav_menu_object( 'Footer menu EN' );
+
+		// If it doesn't exist, let's create it.
+		if( !$menu_exists){
+			$menu_id = wp_create_nav_menu( 'Footer menu EN' );
+
+			// Set up default menu items
+			$pages = ['Home', 'Software engineering', 'Company', 'Career'];
+			for ($i=0; $i<count($pages); $i++) {
+				wp_update_nav_menu_item($menu_id, 0, array(
+				'menu-item-title'		=> $pages[$i],
+				'menu-item-object'		=> 'page',
+				'menu-item-object-id'	=> get_page_by_title($pages[$i])->ID,
+				'menu-item-type'		=> 'post_type',
+				'menu-item-status'		=> 'publish'));
+			}
+		};
+
+		wp_nav_menu( array(
+			'theme_location'=> 'footer',
+			'menu'			=> 'Footer menu EN',
+			'container'		=> '',
+			'menu_class'	=> 'nav nav-pills pull-left'
+	));
+	elseif ($currentlang == "de-DE") :
+		// Check if the menu exists
+			$menu_exists = wp_get_nav_menu_object( 'Footer menu DE' );
+
+			// If it doesn't exist, let's create it.
+			if( !$menu_exists ){
+				$menu_id = wp_create_nav_menu( 'Footer menu DE' );
+
+				wp_update_nav_menu_item($menu_id, 0, array(
+					'menu-item-title'		=> __('Add menu items to Footer menu DE here', 'fractal'),
+					'menu-item-url' 		=> get_bloginfo('wpurl') . '/wp-admin/nav-menus.php',
+					'menu-item-status'		=> 'publish')
+				);
+			};
+
+			wp_nav_menu( array(
+				'theme_location'	=> 'footer',
+				'menu'				=> 'Footer menu DE',
+				'container'			=> '',
+				'menu_class'		=> 'nav nav-pills pull-left',
+				'fallback_cb'		=> 'default_footer_nav'
+			));
+	endif;
+};
+add_action( 'wp_create_footer_nav_menu', 'action_wp_create_footer_nav_menu' );
