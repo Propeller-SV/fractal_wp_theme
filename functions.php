b77be81d67df2e8715d8e506d2f719f9e24d5e82
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-23 10:00:23 +0300

    extract fractal_theme_options from functions to separate file. Add error/success botstrap modal windows to wp_mail function

--- functions.php
+++ functions.php
@@ -54,6 +54,13 @@ require_once THEMEFUNC . '/company-career-main-metabox.php';
 
 /**
  * ----------------------------------------------------------------------------------------
+ * Fractal Options Menu Page
+ * ----------------------------------------------------------------------------------------
+ */
+require_once THEMEFUNC . '/fractal_theme_options.php';
+
+/**
+ * ----------------------------------------------------------------------------------------
  * Set the content width based on the theme's design and stylesheet.
  * ----------------------------------------------------------------------------------------
  */
@@ -156,10 +163,24 @@ function deliver_mail() {
 		// If email has been process for sending, display a success message
 		if ( wp_mail( $to, $subject, $message, $headers ) ) {
 
-			echo "<script type='text/javascript'>alert('Thanks for contacting us, expect a response soon.')</script>";
+			?>
+			<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
+			<script>
+				$(document).ready(function() {
+				    $(".success").trigger('click');
+				});
+			</script>
+			<?php
 
 		} else {
-			echo "<script type='text/javascript'>alert('An unexpected error occurred.')</script>";
+			?>
+			<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
+			<script>
+				$(document).ready(function() {
+				    $(".error").trigger('click');
+				});
+			</script>
+			<?php
 		}
 	}
 }
@@ -186,96 +207,6 @@ function is_blog () {
 	$posttype = get_post_type($post);
 	return ( ((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ( $posttype == 'post')  ) ? true : false ;
 }
-
-/**
- * ----------------------------------------------------------------------------------------
- * Footer social menu links
- * ----------------------------------------------------------------------------------------
- */
-function fractal_options() {
-	add_menu_page( __('Fractal Options', 'fractal'), __('Fractal Options', 'fractal'), 'manage_options', 'fractal_options_page', 'display_fractal_options_page', 'dashicons-admin-generic' );
-}
-function display_fractal_options_page() {
-	?>
-	<div class="wrap">
-		<h2><?php _e('Fractal options', 'fractal'); ?></h2>
-		<form method="post" action="options.php" enctype="multipart/form-data">
-			<?php settings_fields( 'fractal_options_page' ); ?>
-			<?php do_settings_sections( __FILE__ ); ?>
-			<?php submit_button(); ?>
-		</form>
-	</div>
-	<?php
-}
-
-function initialize_fractal_options() {
-
-
-	add_settings_section( 'main_social_section', __('Social Media Settings', 'fractal'), function() {}, __FILE__ );
-	add_settings_section( 'contact_data_section', __('Contact Data', 'fractal'), function() {}, __FILE__ );
-	add_settings_section( 'fractal_theme_settings', __('Theme Settings', 'fractal'), function() {}, __FILE__ );
-
-
-	add_settings_field( 'facebook_link', __('Facebook profile: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[facebook]" value="<?php echo isset($options['facebook']) ? $options['facebook'] : ''; ?>" size="60"><?php
-	}, __FILE__, 'main_social_section' );
-	add_settings_field( 'twitter_link', __('Twitter profile: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[twitter]" value="<?php echo isset($options['twitter']) ? $options['twitter'] : ''; ?>" size="60"><?php
-	}, __FILE__, 'main_social_section' );
-	add_settings_field( 'linked_in_link', __('LinkedIn profile: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[linked_in]" value="<?php echo isset($options['linked_in']) ? $options['linked_in'] : ''; ?>" size="60"><?php
-	}, __FILE__, 'main_social_section' );
-	add_settings_field( 'google_plus_link', __('Google+ profile: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[google_plus]" value="<?php echo isset($options['google_plus']) ? $options['google_plus'] : ''; ?>" size="60"><?php
-	}, __FILE__, 'main_social_section' );
-
-
-	add_settings_field( 'fractal_phone_data', __('Phone: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[phone]" value="<?php echo isset($options['phone']) ? $options['phone'] : ''; ?>" size="40"><?php
-	}, __FILE__, 'contact_data_section' );
-	add_settings_field( 'fractal_email_data', __('Email: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[email]" value="<?php echo isset($options['email']) ? $options['email'] : ''; ?>" size="40"><?php
-	}, __FILE__, 'contact_data_section' );
-	add_settings_field( 'fractal_address_data', __('Address: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="text" name="fractal_options[address]" value="<?php echo isset($options['address']) ? $options['address'] : ''; ?>" size="60"><?php
-	}, __FILE__, 'contact_data_section' );
-	add_settings_field( 'fractal_how_to_get_data', __('How to get: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><textarea name="fractal_options[how_to_get]" cols="60"><?php echo isset($options['how_to_get']) ? $options['how_to_get'] : ''; ?></textarea><?php
-	}, __FILE__, 'contact_data_section' );
-
-
-	add_settings_field( 'contactform_background_image', __('Contactform Background: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="file" id="contactform_background_image" name="contactform_background_image" size="25" /><br>
-		<input type="text" name="fractal_options[contactform_background_image]" value="<?php echo $options['contactform_background_image']; ?>" size="80"><br>
-		<img src="<?php echo $options['contactform_background_image']; ?>" alt='...' width="250"><?php
-	}, __FILE__, 'fractal_theme_settings' );
-	add_settings_field( 'special_offer_status', __('Enable Special Offer: ', 'fractal'), function() { $options = get_option( 'fractal_options' );
-		?><input type="checkbox" name="fractal_options[special_offer]" <?php echo (isset($options['special_offer']) && $options['special_offer']) ? 'checked' : ''; ?>><?php
-	}, __FILE__, 'fractal_theme_settings' );
-
-	register_setting( 'fractal_options_page', 'fractal_options', 'fractal_validate' );
-
-}
-
-// Sanitize URLs to add to database
-function fractal_validate($options) {
-	// $links = [];
-	// foreach ($url as $key => $link) {
-	// 	$links[$key] = esc_url_raw($link);
-	// }
-	// return $links;
-	if ( !empty($_FILES['contactform_background_image']['tmp_name']) ) {
-		$override = array('test_form' => false);
-		$file = wp_handle_upload($_FILES['contactform_background_image'], $override);
-		$options['contactform_background_image'] = $file['url'];
-	} else {
-		// $options['contactform_background_image'] = $options['contactform_background_image'];
-	}
-	return $options;
-}
-
-add_action( 'admin_menu', 'fractal_options' );
-add_action( 'admin_init', 'initialize_fractal_options' );
 
 /**
  * ----------------------------------------------------------------------------------------
