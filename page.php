63e602405f2e0e944c76298aa18280ab5ef8804f
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-29 10:14:38 +0300

    add banner with page_title to page.php to look like other pages. Center the thumbnail. Convert indent to tab

--- page.php
+++ page.php
@@ -3,24 +3,23 @@
  * The template for displaying pages
  *
  * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
  */
 ?>
 
 <?php get_header(); ?>
-
-	<div class="container" style="margin-top:145px">
-
+	<section class="banner">
+		<div class="container text-uppercase">
+			<ol>
+				<li><?php the_title(); ?></li>
+			</ol>
+		</div>
+	</section>
+	<div class="container">
 		<?php while ( have_posts() ) : the_post(); ?>
-
-		<?php the_post_thumbnail('thumbnail'); ?>
-		<h1><?php the_title(); ?></h1>
-		<p><?php the_content(); ?></p>
-
+			<div class="text-center">
+				<?php the_post_thumbnail('large'); ?>
+			</div>
+			<p><?php the_content(); ?></p>
 		<?php endwhile; ?>
-
 	</div>
-
-
 <?php get_footer();?>
