169c3571969f45aff10b1da43c5e149dc9474a91
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-29 12:46:11 +0300

    Clean up index.php. Use endif, endfor etc. Use "<?=". Localize string. Convert indent to tab

--- index.php
+++ index.php
@@ -12,18 +12,17 @@
 ?>
 
 <?php get_header(); ?>
-
 	<section class="banner">
 		<div class="container">
 			<ul>
-				<li><?php
-				if ( get_option('page_for_posts') ) {
+				<li>
+				<?php if ( get_option('page_for_posts') ) {
 					$blog_page_id = get_option('page_for_posts');
 					echo '<a href="' . get_permalink( $blog_page_id ) . '">' . get_page($blog_page_id)->post_title . '</a>';
 				} else {
 					echo '<a href="' . get_bloginfo( 'url' ) . '">BLOG</a>';
-				}
-				?></li>
+				} ?>
+				</li>
 			</ul>
 		</div>
 	</section>
@@ -39,57 +38,57 @@
 							$url = $thumb['0'];
 							?>
 							<?php if ($url) : ?>
-							<img src="<?php echo $url; ?>" class="human-img" />
+							<img src="<?= $url; ?>" class="human-img" />
 							<?php endif; ?>
 
 							<h1 class="text-center"><?php the_title(); ?></h1>
 
 							<ul class="list-top text-center">
-								<li><a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>"><img src="<?php echo IMAGES; ?>/icon-user.png" /> <?php the_author(); ?> /</a></li>
-								<li><a href="#"><img src="<?php echo IMAGES; ?>/icon-clock.png" /><?php the_time( ' F d.m.Y /' ); ?></a></li>
-								<li style="color:#aaa; font-size:10px"><img src="<?php echo IMAGES; ?>/icon-folder.png" /> <?php echo get_the_category_list( ', ' ); ?></li>
+								<li><a href="<?= get_author_posts_url( get_the_author_meta('ID') ); ?>"><img src="<?= IMAGES; ?>/icon-user.png" /> <?php the_author(); ?> /</a></li>
+								<li><a href="#"><img src="<?= IMAGES; ?>/icon-clock.png" /><?php the_time( ' F d.m.Y /' ); ?></a></li>
+								<li><img src="<?= IMAGES; ?>/icon-folder.png" /> <?= get_the_category_list( ', ' ); ?></li>
 							</ul>
 
 							<?php the_excerpt(); ?>
 
-							<p class="continue-read-link text-center"><a href="<?php the_permalink(); ?>">Continue Read</a></p>
+							<p class="continue-read-link text-center"><a href="<?php the_permalink(); ?>"><?php _e('Continue Read', 'fractal'); ?></a></p>
 
 							<ul class="list-bottom">
-								<li><a href="#"><img src="<?php echo IMAGES; ?>/icon-comments.png" /></a></li>
-								<li><a href="#"><img src="<?php echo IMAGES; ?>/icon-oy.png" /></a></li>
+								<li><a href="#"><img src="<?= IMAGES; ?>/icon-comments.png" /></a></li>
+								<li><a href="#"><img src="<?= IMAGES; ?>/icon-oy.png" /></a></li>
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-										<img src="<?php echo IMAGES; ?>/icon-forward.png" />
+										<img src="<?= IMAGES; ?>/icon-forward.png" />
 									</a>
 									<ul class="dropdown-menu">
-										<li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>"><img src="<?php echo IMAGES; ?>/linkedin-color.png" /></a></li>
-										<li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" target="blank"><img src="<?php echo IMAGES; ?>/facebook-color.png" /></a></li>
-										<li><a href="https://twitter.com/intent/tweet?text=<?php the_permalink();?>"><img src="<?php echo IMAGES; ?>/twitter-color.png" /></a></li>
-										<li><a href="https://plus.google.com/share?url=<?php the_permalink();?>"><img src="<?php echo IMAGES; ?>/google-color.png" /></a></li>
+										<li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>"><img src="<?= IMAGES; ?>/linkedin-color.png" /></a></li>
+										<li><a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" target="blank"><img src="<?= IMAGES; ?>/facebook-color.png" /></a></li>
+										<li><a href="https://twitter.com/intent/tweet?text=<?php the_permalink();?>"><img src="<?= IMAGES; ?>/twitter-color.png" /></a></li>
+										<li><a href="https://plus.google.com/share?url=<?php the_permalink();?>"><img src="<?= IMAGES; ?>/google-color.png" /></a></li>
 									</ul>
 								</li>
 							</ul>
 						</div>
 					<?php endwhile; ?>
 					<!-- Pagination -->
-					<div class="list-pagination text-center">
-						<ul class="pagination pagination-lg">
-							<?php
-							$args = array(
-								'prev_text'	=> '&laquo;',
-								'next_text'	=> '&raquo;',
-								'type'		=> 'array',
-								'mid_size'	=> '1'
-								);
-							for ($i=0; $i<count(paginate_links($args)); $i++) {
-								$blog_page = paginate_links( $args );
-								echo '<li>' . $blog_page[$i] . '</li>';
-							}
-							?>
-						</ul>
-					</div>
+						<div class="list-pagination text-center">
+							<ul class="pagination pagination-lg">
+								<?php
+								$args = array(
+									'prev_text'	=> '&laquo;',
+									'next_text'	=> '&raquo;',
+									'type'		=> 'array',
+									'mid_size'	=> '1'
+									);
+								for ($i=0; $i<count(paginate_links($args)); $i++) {
+									$blog_page = paginate_links( $args );
+									echo '<li>' . $blog_page[$i] . '</li>';
+								}
+								?>
+							</ul>
+						</div>
 					<?php else : ?>
-					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 					<?php endif; ?>
 				</div><!-- end of col-sm-9 -->
 				<div class="col-xs-12 col-sm-3">
@@ -137,7 +136,7 @@
 							<h3><span>S</span>ubscribe now if you want to recieve updates and news via email.</h3>
 							<div class="paper-plane">
 								<h3><span>E</span>nter your email...</h3>
-								<img src="<?php echo IMAGES; ?>/paper-plane.png" class="pull-right" />
+								<img src="<?= IMAGES; ?>/paper-plane.png" class="pull-right" />
 							</div>
 						</div>
 					</div><!-- end of fragment-human -->
@@ -145,26 +144,22 @@
 						<div class="heading-post">
 							<h3 class="text-center"><?php _e('Popular Posts', 'fractal'); ?></h3>
 						</div>
-						<?php
-							$most_viewed_posts = new WP_Query( array('v_sortby' => 'views', 'v_orderby' => 'DESC', 'showposts' => 3) );
-							if ($most_viewed_posts->have_posts()) : while ($most_viewed_posts->have_posts()) : $most_viewed_posts->the_post();
-							$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(130,130) );
-							$url = $thumb['0']; ?>
-							<div class="popular-post">
-								<a href="<?php the_permalink(); ?>"><h4 class="text-center"><?php the_title(); ?></h4></a>
-								<p class="text-center"><i><?php the_time( 'F d, Y'); ?></i></p>
-								<?php if ($url) { ?>
-									<img src="<?php echo $url; ?>" class="human-img" />
-								<?php } ?>
-							</div>
-							<?php
-							endwhile; endif;
-							wp_reset_postdata();
-						?>
+						<?php $most_viewed_posts = new WP_Query( array('v_sortby' => 'views', 'v_orderby' => 'DESC', 'showposts' => 3) ); ?>
+						<?php if ($most_viewed_posts->have_posts()) : while ($most_viewed_posts->have_posts()) : $most_viewed_posts->the_post(); ?>
+							<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(130,130) ); ?>
+							<?php $url = $thumb['0']; ?>
+								<div class="popular-post">
+									<a href="<?php the_permalink(); ?>"><h4 class="text-center"><?php the_title(); ?></h4></a>
+									<p class="text-center"><i><?php the_time( 'F d, Y'); ?></i></p>
+									<?php if ($url): ?>
+										<img src="<?= $url; ?>" class="human-img" />
+									<?php endif; ?>
+								</div>
+						<?php endwhile; endif; ?>
+						<?php wp_reset_postdata(); ?>
 					</div><!-- end of fragment-human -->
 				</div><!-- end of col-xs-12 col-sm-3 -->
 			</div><!-- end of row -->
 		</div><!-- end of container -->
 	</section>
-
 <?php get_footer();?>
