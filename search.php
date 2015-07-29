a70233c14c98cc02ffe953f9cafecbd5e3ccbb57
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-29 10:12:00 +0300

    add banner to search_result_page(search.php) to look like other pages. Convert indent to tab

--- search.php
+++ search.php
@@ -5,20 +5,18 @@
 ?>
 
 <?php get_header(); ?>
-
-	<div class="container" style="margin-top:145px">
-		<?php if ( have_posts() ) : ?>
-	
+	<section class="banner">
+		<div class="container">
 			<h2><?php _e('Search results for: ', 'fractal'); ?><?php the_search_query(); ?></h2>
-			
+		</div>
+	</section>
+	<div class="container">
+		<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-		<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-		<p><?php the_excerpt(); ?></p>
-
-		<?php endwhile; else: ?>
-			<p><?php _e( 'Sorry, no posts found.', 'fractal' ); ?></p>
+				<h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+				<p><?php the_excerpt(); ?></p>
+			<?php endwhile; else: ?>
+				<p><?php _e( 'Sorry, no posts found.', 'fractal' ); ?></p>
 		<?php endif; ?>
 	</div>
-
 <?php get_footer();?>
