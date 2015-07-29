b98e0318638d68fa77e5a56bf5943652840aeb38
Serhii Prokhorenko <grey.psv@gmail.com>
2015-07-29 10:05:37 +0300

    convert indentation to tab in sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 /**
  * sidebar.php
  */
 ?>
 
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-	
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	
+
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
 <?php endif; ?>
